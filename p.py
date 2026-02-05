b118270c67419812b873559252731da0dbedc48a
picado <wasimmnurewale@gmail.com>
2026-02-05 02:03:14 -1200

    added_new_files

--- p.py
+++ p.py
@@ -1,1 +1,31 @@
-print("heloo")
+# varible diclaration
+a = 10
+b = 20
+# addition
+c = a + b
+print(" +the sum is", c)
+# substraction
+d = a - b
+
+print(" - the difference is", d)
+# multiplication
+e = a * b
+print("* the product is", e)
+# division
+f = a / b
+
+print("/ the quotient is", f)
+# modulus
+g = a % b
+print("% the remainder is", g)
+# exponentiation
+h = a ** b
+print("** the result of a raised to the power of b is", h)
+# floor division
+i = a // b
+print("// the result of floor division is", i)
+
+a = a = a = 10
+a,b,c,d,e,f,g,h,i = 10,20,30,40,50,60,70,80,90
+print(a,b,c,d,e,f,g,h,i)
+
